ba47a58340470c33e56206d769eda2e204522092
wsco <wwscoggin@gmail.com>
2019-07-17 07:59:19 -0700

    added exception to args

--- app.py
+++ app.py
@@ -10,20 +10,28 @@ app = Flask(__name__)
 api = Api(app)
 
 class getImage(Resource):
-    def get(self, imageName):        
-        args = request.args
-        
-        loadTime = args['load']
+    def get(self, imageName):
+
+        if request.args: 
+            try:
+                loadTime = request.args['load']
+            except IndexError:
+                loadTime = 0
 
         outImg = Image.new('RGBA', (256, 256), (255, 0, 0, 0))
+
         url = 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Cylon_Centurion_head.jpg'
+        
         response = requests.get(url)
+        
         if(response):
         # if no response this return will not happen. return outside if statement will return stream of empty Out image instantiated above
             time.sleep(float(loadTime))
             return send_file(BytesIO(response.content), mimetype='image/png')
+        else:
+            return send_file(BytesIO(outImg), mimetype = 'image/png') 
 
 api.add_resource(getImage,"/getImage/<string:imageName>")
 
 #Uncomment below and run in terminal with 'python app.py' for local webserver using flask dev
-#app.run(debug=True)
+# app.run(debug=True)
